3d3a3d1e7b5339132461bbd0f8d4dc441977f0f7
Max Zykin <sanchez.max@yandex.ru>
2023-04-18 11:23:35 +0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Aleksey Novikov on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
